63e109855e016410f8c7e06215dee181e96057c7
Liz Schuering <lschuering@.gmail.com>
2021-02-20 15:47:14 -0600

    parent middleware

--- myApp.js
+++ myApp.js
@@ -4,23 +4,25 @@ const app = express();
 const ninetyDaysInSeconds = 90*24*60*60;
 const timeInSeconds = ninetyDaysInSeconds;
 
-app.use(helmet.hidePoweredBy());
+app.use(helmet());
 
-app.use(helmet.frameguard({action: 'deny'}));
+// app.use(helmet.hidePoweredBy());
 
-app.use(helmet.xssFilter());
+// app.use(helmet.frameguard({action: 'deny'}));
 
-app.use(helmet.noSniff());
+// app.use(helmet.xssFilter());
 
-app.use(helmet.ieNoOpen());
+// app.use(helmet.noSniff());
 
-app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
+// app.use(helmet.ieNoOpen());
 
-app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 
-app.use(helmet.noCache());
+// app.use(helmet.dnsPrefetchControl());
 
-app.use(helmet.contentSecurityPolicy({ directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com'],}}));
+// app.use(helmet.noCache());
+
+// app.use(helmet.contentSecurityPolicy({ directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com'],}}));
 
 
 
